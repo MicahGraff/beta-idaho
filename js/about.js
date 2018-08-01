83705084862ce6743977c3c54ffe9a43fd2089b0
Micah Graff <micah@graffs.com>
2018-07-31 23:50:51 -0700

    Finished About Beta Page

--- js/about.js
+++ js/about.js
@@ -1,68 +1,46 @@
 $(document).ready(function() {
 
 $(window).scroll(function(e){
-	if ($(this).scrollTop() <= 500)
+	if ($(this).scrollTop() <= 800)
   	{ 
       $('body').css('background','url("../images/aboutImages/betaidaho.jpg") repeat center fixed').fadeIn('slow');
       $('body').css('background-size','cover').fadeIn('slow');
-
-      $('#aboutarrowtop').fadeOut(200);
-      $('#aboutarrowmiddle').fadeOut(200);
   	}
-  	if ($(this).scrollTop() > 500 && $(this).scrollTop() <= 1150)
+  	if ($(this).scrollTop() > 800 && $(this).scrollTop() <= 1700)
   	{ 
     	$('body').css('background','url("../images/aboutImages/ww2.jpg") repeat center fixed').fadeIn('slow');
       $('body').css('background-size','cover').fadeIn('slow');
-
-      $('#aboutarrowtop').fadeIn(200);
-      $('#aboutarrowmiddle').fadeOut(200);
   	}
-  	if ($(this).scrollTop() > 1150 && $(this).scrollTop() <= 1800)
+  	if ($(this).scrollTop() > 1700 && $(this).scrollTop() <= 2500)
   	{
     	$('body').css('background','url("../images/aboutImages/oldmain.jpg") repeat center fixed').fadeIn('slow');
       $('body').css('background-size','cover').fadeIn('slow');
-
-      $('#aboutarrowmiddle').fadeIn(200);
-      $('#aboutarrowtop').fadeOut(200);
   	}
-  	if ($(this).scrollTop() > 1800)
+  	if ($(this).scrollTop() > 2500)
   	{
     	$('body').css('background','url("../images/aboutImages/betaflag.jpg") repeat center fixed').fadeIn('slow');
       $('body').css('background-size','cover').fadeIn('slow');
-
-      $('#aboutarrowtop').fadeOut(200);
-      $('#aboutarrowmiddle').fadeOut(200);
   	}
 });
 
 $('#ggHist').click(function() {
-	$('html,body').animate({
-        scrollTop: $("#gghistoryscroll").offset().top},
-        'slow');
+  $("html, body").animate({ scrollTop: "900px" }, 'slow');
 });
 
 $('#btpHist').click(function() {
-	$('html,body').animate({
-        scrollTop: $("#btphistoryscroll").offset().top},
-        'slow');
+	$("html, body").animate({ scrollTop: "1800px" }, 'slow');
 });
 
 $('#mopIn').click(function() {
-	$('html,body').animate({
-        scrollTop: $("#mopinitiativescroll").offset().top},
-        'slow');
+	$("html, body").animate({ scrollTop: "2700px" }, 'slow');
 });
 
 $('#aboutarrowtop').click(function() {
-  $('html,body').animate({
-        scrollTop: $("#btphistoryscroll").offset().top},
-        'slow');
+  $("html, body").animate({ scrollTop: "1800px" }, 'slow');
 });
 
 $('#aboutarrowmiddle').click(function() {
-  $('html,body').animate({
-        scrollTop: $("#mopinitiativescroll").offset().top},
-        'slow');
+  $("html, body").animate({ scrollTop: "2700px" }, 'slow');
 });
 
 });
